a5d89af4b698c34468d3d9cb26298147fb38a6f5
Ghosthsia <145263586+Ghosthsia@users.noreply.github.com>
2024-05-02 03:39:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-FFcrypto
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+
+@app.route('/encrypt', methods=['GET'])
+def encrypt():
+    input_id = request.args.get('id')
+    encrypted_id = Encrypt_ID(input_id)
+    return jsonify({'encrypted_id': encrypted_id})
+
+if __name__ == '__main__':
+    app.run(debug=True)
